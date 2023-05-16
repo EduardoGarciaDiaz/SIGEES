7fca0625251944237133b2ca7e82072a7d2b950e
Eduardo García Díaz <eduardo.gd17@gmail.com>
2023-05-16 12:14:13 -0600

    Proyecto en JavaFX creado

---
+++ JavaFXSIGEES/src/javafxsigees/controladores/FXMLSIGEESController.java
@@ -0,0 +1,32 @@
+/*
+ * Autor: Eduardo García Díaz
+ * Fecha de creación: 16/05/2023
+ * Descripción: Controlador de FXMLSIGEES
+ */
+
+package javafxsigees.controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class FXMLSIGEESController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
