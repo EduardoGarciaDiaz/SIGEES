d4a39c7712c517b985ad623df32bd05b14951198
Eduardo García Díaz <eduardo.gd17@gmail.com>
2023-05-16 20:34:43 -0600

    Creación de archivos de vista y controlador del inicio del administrador
    Se creó la vista de inicio (temporal) del administrador, además de que se creó su controlador pero aún sin implementación

---
+++ JavaFXSIGEES/src/javafxsigees/controladores/FXMLInicioAdministradorController.java
@@ -0,0 +1,43 @@
+/*
+ * Autor: Eduardo García Díaz
+ * Fecha de creación: 15/05/2023
+ * Descripción: Controlador de la vista del inicio del administrador
+ */
+
+package javafxsigees.controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+public class FXMLInicioAdministradorController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //TODO
+    }    
+
+    @FXML
+    private void clicAdministrarTarjetas(ActionEvent event) {
+        System.out.println("No disponible por el momento");
+    }
+
+    @FXML
+    private void clicAdministrarUsuarios(ActionEvent event) {
+        System.out.println("No disponible por el momento");
+    }
+
+    @FXML
+    private void clicAdministrarCuotas(ActionEvent event) {
+        System.out.println("No disponible por el momento");
+    }
+
+    @FXML
+    private void clicConsultarHistorico(ActionEvent event) {
+        //TODO
+    }
+    
+}
