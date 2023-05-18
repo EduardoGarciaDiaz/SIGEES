d170e66f2d008cf0088db3576baaf80cb0a17cb2
Eduardo García Díaz <eduardo.gd17@gmail.com>
2023-05-17 19:29:03 -0600

    Estilo agregado a botones
    Se agregó el estilo a los botones del inicio del administrador

--- JavaFXSIGEES/src/javafxsigees/controladores/FXMLInicioAdministradorController.java
+++ JavaFXSIGEES/src/javafxsigees/controladores/FXMLInicioAdministradorController.java
@@ -11,9 +11,13 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 public class FXMLInicioAdministradorController implements Initializable {
+
+    @FXML
+    private Button btnInicio;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -23,12 +27,12 @@ public class FXMLInicioAdministradorController implements Initializable {
     @FXML
     private void clicAdministrarTarjetas(ActionEvent event) {
         System.out.println("No disponible por el momento");
+        
     }
 
     @FXML
     private void clicAdministrarUsuarios(ActionEvent event) {
-        System.out.println("No disponible por el momento");
-    }
+        System.out.println("No disponible por el momento");    }
 
     @FXML
     private void clicAdministrarCuotas(ActionEvent event) {
@@ -39,5 +43,24 @@ public class FXMLInicioAdministradorController implements Initializable {
     private void clicConsultarHistorico(ActionEvent event) {
         //TODO
     }
+    
+    @FXML
+    private void sobreTarjeta(MouseEvent event) {
+        Button b = (Button) event.getSource();
+        b.setStyle("-fx-background-color:#dae7f3;");
+        b.setScaleX(1.02);
+        b.setScaleY(1.02);
+         
+    }
+
+    @FXML
+    private void fueraTarjeta(MouseEvent event) {
+        Button b = (Button) event.getSource();
+        b.setStyle("-fx-background-color: #FFFFFF; -fx-background-radius: 13px;");
+        b.setScaleX(1);
+        b.setScaleY(1);
+    }
+
+    
     
 }
