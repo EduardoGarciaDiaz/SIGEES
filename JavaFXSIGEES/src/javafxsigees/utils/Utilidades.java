e05ae86a728f6d03a5b4fb56c0b6a88f5a0d11df
Eduardo García Díaz <eduardo.gd17@gmail.com>
2023-05-18 19:56:35 -0600

    Se implementó método para inicializar escenas en la clase Utilidades

--- JavaFXSIGEES/src/javafxsigees/utils/Utilidades.java
+++ JavaFXSIGEES/src/javafxsigees/utils/Utilidades.java
@@ -6,16 +6,31 @@
 
 package javafxsigees.utils;
 
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 
 public class Utilidades {
     
-    public static void mostrarDialogoSimple(String titulo, String mensaje, Alert.AlertType tipo){
+    public static void mostrarDialogoSimple(String titulo, String mensaje, Alert.AlertType tipo) {
         Alert alertaSimple = new Alert(tipo);
         alertaSimple.setTitle(titulo);
         alertaSimple.setContentText(mensaje);
         alertaSimple.setHeaderText(null);
         alertaSimple.showAndWait();
+    }
+    
+    public static Scene inicializarEscena(String ruta) {
+        Scene escena = null;
+        try {
+            Parent vista = FXMLLoader.load(javafxsigees.JavaFXSIGEES.class.getResource(ruta));
+            escena = new Scene(vista);
+        } catch (IOException ex) {
+            System.out.println("ERROR: " + ex.getMessage());
+        }
+        return escena;
     }
     
 }
